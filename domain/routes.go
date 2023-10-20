70ad61d3343d92c2ecd1881ba1872cdd1686b860
Mark Gerald Martins <mark.g.martins@gmail.com>
2023-10-19 22:18:48 -0300

    Final grooming

---
+++ domain/routes.go
@@ -0,0 +1,31 @@
+package domain
+
+import (
+	"encoding/json"
+	"github.com/markgerald/flyroutes/models"
+	"io"
+	"net/http"
+)
+
+const routesUrl = "https://services-api.ryanair.com/views/locate/3/routes"
+
+func GetRoutesData() ([]models.Route, error) {
+	resp, err := http.Get(routesUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var allRoutes []models.Route
+	err = json.Unmarshal(body, &allRoutes)
+	if err != nil {
+		return nil, err
+	}
+
+	return allRoutes, nil
+}
---
+++ domain/schedule.go
@@ -0,0 +1,38 @@
+package domain
+
+import (
+	"encoding/json"
+	"github.com/markgerald/flyroutes/models"
+	"io"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const scheduleURL = "https://services-api.ryanair.com/timtbl/3/schedules/"
+
+func GetScheduleData(departure, arrival string) (models.Schedule, error) {
+	year, month, _ := time.Now().Date()
+	url := scheduleURL +
+		departure + "/" +
+		arrival + "/years/" +
+		strconv.Itoa(year) + "/months/" + strconv.Itoa(int(month))
+	resp, err := http.Get(url)
+	if err != nil {
+		return models.Schedule{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return models.Schedule{}, err
+	}
+
+	var schedule models.Schedule
+	err = json.Unmarshal(body, &schedule)
+	if err != nil {
+		return models.Schedule{}, err
+	}
+
+	return schedule, nil
+}
